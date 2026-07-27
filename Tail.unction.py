9163e59244c55d384fc69bd11760894391df4d8a
Tech57625 <tech57625@gmail.com>
2026-07-27 12:02:20 +0530

    Add files via upload

---
+++ Tail.unction.py
@@ -0,0 +1,10 @@
+
+
+def fun(count):
+    if count == 4:
+        return
+
+    fun(count + 1)
+    print("Raghav")
+
+fun(0)
---
+++ print mant time.py
@@ -0,0 +1,8 @@
+def func(count):
+    if count == 4:
+        return
+
+    print("Raghav")
+    func(count + 1)
+
+func(1)  # this is a better approch
